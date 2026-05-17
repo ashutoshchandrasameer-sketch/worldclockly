83e49851ce4d8441641fe779b927de7b8f9e11ba
Techmsameer-Git <techmsameer@gmail.com>
2026-05-18 02:56:26 +0530

    Updates made to UI-02

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
-import { QueryClient } from '@tanstack/react-query'
-import { routeTree } from './routeTree.gen' 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { QueryClient } from "@tanstack/react-query";
 
+import { routeTree } from "./routeTree.gen";
+import "./styles.css";
 
-// 1. Initialize the router instance
-const queryClient = new QueryClient()
-const router = createRouter({ routeTree, context: { queryClient } })
+const queryClient = new QueryClient();
 
-// 2. Register the router type contexts
-declare module '@tanstack/react-router' {
+const router = createRouter({
+  routeTree,
+  context: { queryClient },
+});
+
+declare module "@tanstack/react-router" {
   interface Register {
-    router: typeof router
+    router: typeof router;
   }
 }
 
-// 3. Select the #root element and physically append the application view
-const rootElement = document.getElementById('root')!
-if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement)
-  root.render(
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>
-  )
-}
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
